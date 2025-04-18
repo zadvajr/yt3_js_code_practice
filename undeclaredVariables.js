0c929e117373c563f46f028896efd128020f6f0a
unknown <danielzadva@gmail.com>
2025-04-18 03:00:11 +0100

    undeclared variable

---
+++ undeclaredVariables.js
@@ -0,0 +1,8 @@
+//In Js you can create a variable without declaring it as follows
+x = 5;
+y = 7;
+z = x + y;
+console.log(`${x} + ${y} = ${z}`);
+
+//Note: It is considered considered a good practice to always declare a variable before use.
+// It is also a good practice to declare all your variables at the beginning of the script
