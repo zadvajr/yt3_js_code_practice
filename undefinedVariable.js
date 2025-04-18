953d03a44bf32fd18f1f592e28e095c3550d7386
unknown <danielzadva@gmail.com>
2025-04-18 03:51:14 +0100

    data type

---
+++ undefinedVariable.js
@@ -0,0 +1,12 @@
+//In Js if you declare a variable and you did not assign value to it, that variable will be of type undefined and value of undefined.
+
+//Example
+let x;
+console.log(`The datatype of 'x' is ${typeof x} and the value is '${x}'`);
+
+let y = undefined;
+console.log(`Type of y: ${typeof y}, value: ${y}`);
+
+z = "";
+console.log(typeof z);
+console.log(z);
